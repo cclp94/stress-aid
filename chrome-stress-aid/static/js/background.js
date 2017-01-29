06565e805223a8a4048c3edc93550eed13984ce1
cclp94 <cclp94@gmail.com>
2017-01-29 00:17:53 -0500

    create user token id

--- chrome-stress-aid/static/js/background.js
+++ chrome-stress-aid/static/js/background.js
@@ -21,3 +21,48 @@ chrome.runtime.onMessage.addListener(function (msg, sender) {
     http.send(params);
   }
 });
+
+
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
+}
+
+chrome.storage.sync.get('userid', function(items) {
+    var userid = items.userid;
+    console.log(userid);
+    if (!userid) {
+        userid = getRandomToken();
+         console.log(userid);
+        chrome.storage.sync.set({userid: userid}, function() {
+            useToken(userid);
+        });
+    }
+    function useToken(userid) {
+        // TODO: Use user id for authentication or whatever you want.
+        // Create user on backend
+        var http = new XMLHttpRequest();
+        var url = "http://104.198.249.148:5000/insertuser";
+        var params = JSON.stringify({
+          'user_id': userid
+        });
+        http.open("POST", url, true);
+
+        //Send the proper header information along with the request
+        http.setRequestHeader("Content-type", "application/json");
+
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4 && http.status == 200) {
+                alert(http.responseText);
+            }
+        }
+        http.send(params);
+    }
+});
